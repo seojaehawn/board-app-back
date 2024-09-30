897b6e3c3c4bd65fc355a05ec2a8affe500a2aec
bitcamp\BIT <0215bitcamp@gmail.com>
2024-09-30 10:27:29 +0900

    no message

--- src/test/java/com/example/react_boards_backend/ReactBoardsBackendApplicationTests.java
+++ src/test/java/com/example/react_boards_backend/ReactBoardsBackendApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.react_boards_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ReactBoardsBackendApplicationTests {
 
     @Test
